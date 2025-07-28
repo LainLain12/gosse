61f9e41eccbf8a0ffd71ed4503e67582dafd81f9
LainLain12 <minnthuri3inhtun@gmail.com>
2025-07-28 22:20:13 +0630

    Change server port to 4597 and update geminisocket clients count

--- chat/livechat.go
+++ chat/livechat.go
@@ -7,7 +7,6 @@ import (
 	"time"
 )
 
-// ChatSSEHandler streams chat messages via SSE
 func ChatSSEHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
@@ -20,16 +19,18 @@ func ChatSSEHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// On first connect, send all 50 messages as a JSON array
+	// On first connect, send all 50 messages as SSE events (data: ...)
 	chatMu.Lock()
 	all := make([]any, len(chatMessages))
 	copy(all, chatMessages)
 	chatMu.Unlock()
-	b, err := json.Marshal(all)
-	if err == nil {
-		fmt.Fprintf(w, "%s\n", string(b))
-		flusher.Flush()
+	for _, msg := range all {
+		b, err := json.Marshal(msg)
+		if err == nil {
+			fmt.Fprintf(w, "data: %s\n\n", string(b))
+		}
 	}
+	flusher.Flush()
 
 	// Track the last sent message index
 	lastIdx := len(all)
@@ -50,7 +51,7 @@ func ChatSSEHandler(w http.ResponseWriter, r *http.Request) {
 				for _, msg := range chatMessages[lastIdx:] {
 					b, err := json.Marshal(msg)
 					if err == nil {
-						fmt.Fprintf(w, "%s\n", string(b))
+						fmt.Fprintf(w, "data: %s\n\n", string(b))
 					}
 				}
 				flusher.Flush()
@@ -64,3 +65,61 @@ func ChatSSEHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 }
+
+// // ChatSSEHandler streams chat messages via SSE
+// func ChatSSEHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/event-stream")
+// 	w.Header().Set("Cache-Control", "no-cache")
+// 	w.Header().Set("Connection", "keep-alive")
+// 	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+// 	flusher, ok := w.(http.Flusher)
+// 	if !ok {
+// 		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	// On first connect, send all 50 messages as a JSON array
+// 	chatMu.Lock()
+// 	all := make([]any, len(chatMessages))
+// 	copy(all, chatMessages)
+// 	chatMu.Unlock()
+// 	b, err := json.Marshal(all)
+// 	if err == nil {
+// 		fmt.Fprintf(w, "%s\n", string(b))
+// 		flusher.Flush()
+// 	}
+
+// 	// Track the last sent message index
+// 	lastIdx := len(all)
+
+// 	// Broadcast new messages as they arrive (as single JSON object, not array)
+// 	notify := r.Context().Done()
+// 	msgTicker := time.NewTicker(1 * time.Second)
+// 	pingTicker := time.NewTicker(15 * time.Second)
+// 	defer msgTicker.Stop()
+// 	defer pingTicker.Stop()
+// 	for {
+// 		select {
+// 		case <-notify:
+// 			return
+// 		case <-msgTicker.C:
+// 			chatMu.Lock()
+// 			if lastIdx < len(chatMessages) {
+// 				for _, msg := range chatMessages[lastIdx:] {
+// 					b, err := json.Marshal(msg)
+// 					if err == nil {
+// 						fmt.Fprintf(w, "%s\n", string(b))
+// 					}
+// 				}
+// 				flusher.Flush()
+// 				lastIdx = len(chatMessages)
+// 			}
+// 			chatMu.Unlock()
+// 		case <-pingTicker.C:
+// 			// Send SSE comment as keepalive (ping)
+// 			fmt.Fprintf(w, ": ping\n\n")
+// 			flusher.Flush()
+// 		}
+// 	}
+// }
